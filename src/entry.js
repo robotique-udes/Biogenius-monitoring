df344f35578bd5155dea53df0ee5e71a12abc695
Blain354 <gblain03@hotmail.com>
2022-08-26 17:06:05 -0400

    alert to print

--- src/entry.js
+++ src/entry.js
@@ -48,14 +48,14 @@ socket.onmessage = function(event) {
 
 socket.onclose = function(event) {
   if (event.wasClean) {
-    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
   } else {
-    alert('[close] Connection died');
+    console.log('[close] Connection died');
   }
 };
 
 socket.onerror = function(error) {
-  alert(`[error] ${error.message}`);
+  console.log(`[error] ${error.message}`);
 };
 
 
